35b4519c3135770c931a8a27ab36e098efa0a8ad
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-01-10 19:02:33 +0600

    sohan (10/1/23 7:02pm)

--- src/Pages/Services/BestOffer/BestOffer.jsx
+++ src/Pages/Services/BestOffer/BestOffer.jsx
@@ -1,6 +1,7 @@
 import Offer from '../Offer/Offer';
 import './BestOffer.css';
 import { useEffect, useState } from 'react';
+import serviceImg from '../../../../public/service/service.png'
 
 const BestOffer = () => {
 
@@ -16,15 +17,17 @@ const BestOffer = () => {
     <div>
       <div className='container mx-auto'>
         {/* content section start */}
-        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-10'>
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 md:gap-5 lg:gap-10'>
           {/* offerType section start */}
           <div>
-            {/* title section start */}
-            <h1 className='lg:text-5xl font-extrabold mb-3'>We Gave The Best Offer</h1>
-            <p className='lg:text-2xl font-bold'>Get Special Offer Until 30 December 2022</p>
-            {/* title section end */}
+            <div className='ml-3 lg:ml-0'>
+              {/* title section start */}
+              <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold mb-3'>We Gave The Best Offer</h1>
+              <p className='text-base sm:text-lg md:text-base lg:text-2xl font-bold'>Get Special Offer Until 30 December 2022</p>
+              {/* title section end */}
+            </div>
             {/* offers section start */}
-            <div className='grid grid-cols-1 gap-5'>
+            <div className='grid grid-cols-1 gap-5 mt-4'>
             {
               offers.map((offer) => <Offer
               key={offer.id}
@@ -36,6 +39,9 @@ const BestOffer = () => {
           </div>
           {/* offerType section end */}
           {/* profile section start */}
+          <div>
+            <img className='w-full h-full' src={serviceImg}></img>
+          </div>
           {/* profile section end */}
         </div>
         {/* content section end */}
--- src/Pages/Services/Offer/Offer.jsx
+++ src/Pages/Services/Offer/Offer.jsx
@@ -4,10 +4,19 @@ const Offer = ({offer}) => {
   console.log(offer)
   const {logo, description, title} = offer;
   return (
-    <div className='flex'>
-      <div>
-        <img className='w-[10%]' src={logo}></img>
+    <div className='sm:flex items-center gap-5 bg-slate-500 p-4 sm:p-5 lg:p-6 rounded-xl m-2 lg:m-0'>
+      {/* logo section start */}
+      <div className='flex-1 w-[18%] sm:w-[10%] md:w-[25%] lg:w-[10%] mx-auto'>
+        <img className='w-[100%]' src={logo}></img>
       </div>
+      {/* logo section end */}
+      {/* title & description start */}
+      <div className='flex-2 sm:w-[85%] md:w-[75%] lg:w-[85%] text-center sm:text-left mt-2 sm:mt-0'>
+        <h4 className='text-xl sm:text-2xl md:text-base lg:text-2xl font-bold'>{title}</h4>
+        <p className='text-sm sm:text-lg md:text-sm lg:text-base'>{description}</p>
+      </div>
+      {/* title & description end */}
+      <div></div>
     </div>
   );
 };
