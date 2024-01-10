46ba760bb790b24ba5d49b608b12cc0fdad00c58
Mehadi Hasan <mhshakil06@gmail.com>
2024-01-10 14:19:42 +0600

    add top section

---
+++ src/Pages/Home/TopSection.jsx
@@ -0,0 +1,15 @@
+
+const TopSection = () => {
+  return (
+    <>
+
+    <section className="w-full h-2/6 bg-gray-300">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio eum sapiente commodi maxime, velit quam autem ipsum molestiae. Ipsa, id.</p>
+    </section>
+
+
+    </>
+  )
+}
+
+export default TopSection
