5bc55d46ffbf34c30dce6eaf1a6dea5109f035a6
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-01-11 12:51:14 +0600

    sohan (11/1/24 12:51pm)

---
+++ src/Pages/Contact/ContactForm/ContactForm.jsx
@@ -0,0 +1,28 @@
+import ContactDescription from '../ContactDescription/ContactDescription';
+import Form from '../Form/Form';
+import './ContactForm.css'
+
+const ContactForm = () => {
+  return (
+    <div>
+      <div className='container mx-auto my-10 lg:my-20'>
+        {/* content section start */}
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 md:gap-10'>
+          {/* contactDescription section start */}
+          <div className='m-3'>
+            <ContactDescription></ContactDescription>
+          </div>
+          {/* contactDescription section end */}
+          {/* form section start */}
+          <div className='m-3 bg-slate-400 p-5'>
+            <Form></Form>
+          </div>
+          {/* form section end */}
+        </div>
+        {/* content section end */}
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
