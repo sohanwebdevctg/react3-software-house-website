39e1144b2012ec4a76206422dc03b85298438dd1
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-04-20 16:41:41 +0600

    adding card details page

--- src/Pages/Home/ServiceDetails/ServiceDetails.jsx
+++ src/Pages/Home/ServiceDetails/ServiceDetails.jsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import './ServiceDetails.css'
 
 const ServiceDetails = () => {
 
   const {id} = useParams();
   
   const [details, setDetails] = useState([])
-
-  console.log(details)
+  const navigate = useNavigate()
+  const previous = () => {
+    navigate(-1)
+  }
 
   useEffect(() => {
     fetch('/typeOfService.json')
@@ -20,9 +23,38 @@ const ServiceDetails = () => {
   },[id])
 
   return (
-    <div>
-      <h1>this is service section </h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic fuga fugit minima sunt, error architecto odit? Corporis, excepturi dicta. Quos, dignissimos sapiente tenetur fugiat sint vitae accusamus alias quam totam blanditiis harum ipsum delectus temporibus soluta odio, unde, quasi ea qui nulla dicta ab? Enim magni, laboriosam nihil reprehenderit, magnam exercitationem perferendis quis expedita suscipit sit soluta iste impedit tempore numquam quaerat voluptatum tempora odio sunt. Perferendis ducimus, aut ullam alias asperiores rerum! Aut numquam veritatis esse nisi consequatur cupiditate, dignissimos corrupti odio officiis reprehenderit ipsam expedita nobis enim magnam iste rerum accusamus. Laboriosam voluptatem deserunt sunt repellendus beatae reprehenderit.</p>
+    <div className="my-10 md:mt-24 md:mb-10 ">
+      <div className="container mx-auto px-3 md:px-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* details section start */}
+          <div className="flex-1 space-y-3">
+            <p className="text-xl sm:text-2xl md:text-lg lg:text-2xl text-blue-600/100 font-bold">News</p>
+            <h3 className="text-2xl sm:text-3xl md:text-xl lg:text-3xl text-gray-500">{details.title}</h3>
+            <ul className="flex items-center gap-2">
+            {
+              details?.items?.map((item, index) => <li key={index} className=" bg-blue-600/100 text-white rounded-xl text-xs sm:text-sm md:text-sm lg:text-sm px-2 py-1">{item}
+                </li>)
+            }
+            </ul>
+            <ul className="flex items-center gap-2">
+              <li className="flex items-center gap-2"><img src={details.clientImg} className="w-8 h-8"></img><span className="text-base  sm:text-xl md:text-base lg:text-lg">{details.clientName}</span></li>
+              <li>|</li>
+              <li><span span className="text-base sm:text-xl md:text-base lg:text-lg">{details.date}</span></li>
+            </ul>
+            <p className="text-sm sm:text-base md:text-sm lg:text-base italic lg:pr-5">{details.details}</p>
+            <span className="flex items-center gap-2 cursor-pointer py-1 px-4 bg-blue-600/100 rounded-3xl w-[25%] sm:w-[16%] md:w-[23%] lg:w-[15%] text-sm sm:text-base md:text-sm lg:text-base text-slate-200" onClick={() => previous()}>
+            <Link to="#" >Back </Link>
+            <FaArrowRight/>
+            </span>
+          </div>
+          {/* details section end */}
+          {/* banner section start */}
+          <div className="flex-1 mt-2">
+            <img src={details.image} className="rounded-3xl h-full w-full"></img>
+          </div>
+          {/* banner section end */}
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/TypeOfService/TypeOfService.jsx
+++ src/Pages/Home/TypeOfService/TypeOfService.jsx
@@ -20,8 +20,8 @@ const TypeOfService = () => {
             card.map((data) => <article key={data.id} className='cardArticle my-2 md:my-4'>
               <img src={data.image} className='cardImg w-full h-[90%] lg:h-full rounded-3xl'></img>
               <Link to={`/service-details/${data.id}`} className='cardData w-4/5 bg-slate-100 h-[50%] sm:h-[66%] md:h-[90%] lg:h-[40%] px-8 py-4 sm:py-3 rounded-2xl shadow-lg space-y-1'>
-                <span className='cardDes block text-xs sm:text-sm'>{data.description}</span>
                 <h2 className='cardTitle text-sm sm:test-base text-red-500 font-semibold'>{data.title}</h2>
+                <span className='cardDes block text-xs sm:text-sm'>{data.description}</span>
                 <Link to={`/service-details/${data.id}`} className="cardBtn text-sm text-blue-600 hover:underline">{data.link}</Link>
               </Link>
             </article>)
