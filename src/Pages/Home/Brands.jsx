4dac48e03a059d67ec4a6085be774c1dc6513743
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-04-27 18:25:13 +0600

    changing some data

--- src/Pages/Home/Brands.jsx
+++ src/Pages/Home/Brands.jsx
@@ -1,4 +1,11 @@
-import logo from '../../assets/images/google-logo.jpg';
+
+import img1 from '../../../public/brands/img1.png'
+import img2 from '../../../public/brands/img2.png'
+import img3 from '../../../public/brands/img3.png'
+import img4 from '../../../public/brands/img4.png'
+import img5 from '../../../public/brands/img5.png'
+import img6 from '../../../public/brands/img6.png'
+import img7 from '../../../public/brands/img7.png'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -20,7 +27,7 @@ const Brands = () => {
             <h2 className="text-center text-3xl xl:text-4xl 2xl:text-4xl font-bold tracking-tight text-gray-900 sm:text-2xl mb-2" style={{ textShadow:'2px 2px 5px rgb(99 102 241 / 0.5)' }}>Trusted by <span className='text-blue-600/100'>open</span>, aired out of companies</h2>
             <p className='text-center mb-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, temporibus.</p>
             
-            <div className="mt-6 ">  
+            {/* <div className="mt-6 ">  
                 <Swiper 
                     slidesPerView={5}
                     spaceBetween={30}
@@ -32,17 +39,119 @@ const Brands = () => {
                     }}
                     modules={[FreeMode,Autoplay]}
                     className="mySwiper" >
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
-                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={logo} alt="" style={{ width:"150px", height:"auto" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img1} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img2} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img3} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img4} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img5} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img6} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img7} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img3} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
+                    <SwiperSlide><img className='rounded shadow-md mx-2 mb-5' src={img5} alt="" style={{ width:"150px", height:"100px" }} /></SwiperSlide>
                 </Swiper>
-            </div>
+            </div> */}
+
+<div className="mt-6">
+      <Swiper
+        spaceBetween={20}
+        freeMode={true}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        pagination={{
+          loop: true,
+        }}
+        modules={[FreeMode, Autoplay]}
+        className="mySwiper"
+        breakpoints={{
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 4,
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 5,
+          },
+          // when window width is >= 768px
+          768: {
+            slidesPerView: 6,
+          },
+          // when window width is >= 1024px
+          1024: {
+            slidesPerView: 7,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img1}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img2}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img3}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img4}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img5}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img6}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img7}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='rounded shadow-md mx-2 mb-5'
+            src={img3}
+            alt=""
+            style={{ width: "150px", height: "100px" }}
+          />
+        </SwiperSlide>
+        {/* Include other SwiperSlides similarly */}
+      </Swiper>
+    </div>
+
            </div>
         </div>
       </section>
