6f7eaa775c3977970a42bacc3286c4db013abcb7
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-08-01 11:38:42 +0600

    adding social medial link in footer section

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -15,8 +15,8 @@ const Footer = () => {
             <h3 className="text-sm lg:text-base font-bold mb-2">ABOUT US</h3>
             <ul>
               <li className="text-sm lg:text-base"><Link to="/">Home</Link></li>
-              <li className="text-sm lg:text-base my-1"><Link to="/">Services</Link></li>
-              <li className="text-sm lg:text-base"><Link to="/">Contact</Link></li>
+              <li className="text-sm lg:text-base my-1"><Link to="/services">Services</Link></li>
+              <li className="text-sm lg:text-base"><Link to="/contact">Contact</Link></li>
             </ul>
           </div>
           <div className="m-2">
@@ -39,17 +39,17 @@ const Footer = () => {
           <div className="m-2">
             <h3 className="text-sm lg:text-base font-bold mb-2">FOLLOW US</h3>
             <ul>
-              <li className="text-sm lg:text-base mb-1"><Link to="/">Facebook</Link></li>
-              <li className="text-sm lg:text-base"><Link to="/">Twitter</Link></li>
-              <li className="text-sm lg:text-base my-1"><Link to="/">LinkedIn</Link></li>
-              <li className="text-sm lg:text-base"><Link to="/">Instagram</Link></li>
+              <li className="text-sm lg:text-base mb-1"><Link to="https://www.facebook.com/">Facebook</Link></li>
+              <li className="text-sm lg:text-base"><Link to="https://x.com/login">Twitter</Link></li>
+              <li className="text-sm lg:text-base my-1"><Link to="https://www.linkedin.com/">LinkedIn</Link></li>
+              <li className="text-sm lg:text-base"><Link to="https://www.instagram.com/">Instagram</Link></li>
             </ul>
           </div>
           <div className="m-2">
             <h3 className="text-sm lg:text-base font-bold mb-2">Let's Stay Connected</h3>
             <p className="text-sm lg:text-base sm:my-1 lg:my-1">Enter your Email address</p>
             <form className="flex gap-1">
-              <input name="" className="border-[1px] p-2 md:w-full" type="email" placeholder="enter your email"></input>
+              <input name="" className="border-[1px] p-2 md:w-full focus-within:outline-none" type="email" placeholder="enter your email"></input>
               <button type="submit" className="bg-blue-600/100 text-white px-1 sm:px-2 text-base">Submit</button>
             </form>
           </div>
