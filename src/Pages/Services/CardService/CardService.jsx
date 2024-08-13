bd6754573db0f5e50cd6f7620f5001d791c1d37f
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-08-13 21:48:28 +0600

    responsive the cardservice section in service page

--- src/Pages/Services/CardService/CardService.jsx
+++ src/Pages/Services/CardService/CardService.jsx
@@ -1,59 +1,61 @@
-// import cardImage from '../../assets/images/undraw_watch_application_uhc9.svg'
-import { Link } from 'react-router-dom'
-import cardImage from '../../../assets/images/undraw_watch_application_uhc9.svg'
 import './CardService.css'
 import { useEffect, useState } from 'react'
 
-function CardService() {
+const CardService = () => {
 
-  const [services, setServices] = useState([])
+  const [services, setServices] = useState([]);
 
   useEffect(() => {
-    fetch('https://web.xcode.com.bd/api/v1/service/')
+    fetch('ourServicesData.json')
     .then((res) => res.json())
-    .then((data) => {
-      // console.log(data)
-      setServices(data)
-})
+    .then((data) => setServices(data))
   },[])
 
-  // to do : service(title ,description ,image)
-
-
   return (
-    <div className='lg:mt-20'>
-      <div className="container m-auto p-6 sm:p-4 md:p-0 xl:p-0 2xl:p-0 h-full lg:h-96">
-        <div className='grid grid-cols-1 text-center md:mt-20 mb-2 md:mb-1 lg:mb-3'>
-          <h2 className="text-center font-semibold  md:font-bold xl:font-bold text-xl sm:text-3xl md:text-3xl xl:text-4xl mb-1 " style={{ textShadow:'2px 2px 5px rgb(99 102 241 / 0.5)' }}>Our Service</h2>
+    <div className='mt-6 sm:mt-20 lg:mt-24 xl:mt-28'>
+      <div className="container mx-auto">
+        {/* title section start */}
+        <div className="text-center mb-1 px-5">
+          {/* title section start */}
+          <h2
+            className="text-center font-semibold  md:font-bold xl:font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl mb-1 "
+            style={{ textShadow: "2px 2px 5px rgb(99 102 241 / 0.5)" }}
+          >
+            Our Services
+          </h2>
+          {/* title section end */}
           <div className="flex justify-center mb-2 md:mb-4 lg:mb-4">
-            <h2 className="bg-red-500 w-[32%] sm:w-[32%] md:w-[26%] lg:w-52 h-[1px] rounded"></h2>
+            <h2 className="bg-red-500 w-[32%] sm:w-[23%] md:w-[22%] lg:w-[19%] xl:w-[18%] 2xl:w-[20%] h-[1px] rounded"></h2>
           </div>
-          <p className='mt-2 md:mt-0 text-sm sm:text-base md:text-base lg:text-lg text-slate-700 mb-6'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi amet praesentium.</p>
+          {/* subtitle section start */}
+          <h2
+            className="text-lg sm:text-sm md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold tracking-tight text-gray-900 mb-3"
+            style={{ textShadow: "2px 2px 5px rgb(99 102 241 / 0.5)" }}
+          >
+            Building Your Vision,
+            <span className="text-blue-600/100"> One Step at a Time</span>
+          </h2>
+          {/* subtitle section end */}
+          {/* description section start */}
+          <p className="text-sm sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl text-center">
+            Your success is our priority. We transform complex ideas<br className="hidden sm:block"></br> into solutions that empower your business
+          </p>
+          {/* description section end */}
         </div>
         {/* title section end */}
         {/* card section start */}
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-20 lg:gap-10 content-center mx-auto mt-24 lg:mt-5 h-full md:mx-5 lg:mx-0'>
+        <div className='grid grid-cols-2 sm:grid-cols-5 gap-3 sm:gap-4 md:gap-4 lg:gap-6 xl:gap-7 2xl:gap-7 mt-6 px-5'>
           {
-            services.map((data, index) => <div key={index} className='card bg-indigo-50 w-full h-32 lg:w-64 lg:h-[186px] duration-200 hover:h-36 lg:hover:h-48 rounded-xl relative'>
-            <div className='rounded-xl z-10 absolute left-0 right-0 -top-14 lg:-top-20'>
-              <img className='w-52 h-24 lg:w-40 lg:h-40 mx-auto rounded-xl' src={data.image}></img>
-            </div>
-            <div className='text-center flex justify-center'>
-              <div className='absolute bottom-0 overflow-hidden mb-2'>
-              <h1 className='text-base lg:text-xl'>{data.title}</h1>
-              <Link to="/">
-                <button className="group relative h-10 sm:h-9 md:h-10 lg:h-10   w-24 sm:w-24 md:w-28 lg:w-24 overflow-hidden rounded-lg bg-white text-lg shadow mt-4 mb-2">
-                  <div className="absolute inset-0 w-3 bg-blue-600/100 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-                  <span className="relative text-black group-hover:text-white text-sm sm:text-sm md:text-sm lg:text-base">Start Now</span>
-                </button>
-              </Link>
-              </div>
-            </div>
+            services.map((data, index) => <div key={index} className='text-center rounded-xl shadow-lg hover:shadow-indigo-600/50 cursor-pointer shadow-indigo-500/50 hover:border-solid border-[1px] hover:border-[1px] hover:border-indigo-600 py-3 px-1 lg:py-4 xl:py-5 xl:px-2 space-y-2'>
+            <img src={data.image} className='mx-auto w-7 h-7 sm:w-8 sm:h-8 md:w-8 md:h-8 lg:w-9 lg:h-9 xl:w-12 xl:h-12 2xl:w-14 2xl:h-14'></img>
+            <ul>
+            <li className='text-[11px] sm:text-[10px] md:text-xs lg:text-base xl:text-lg 2xl:text-xl font-bold'>{data.title1}</li>
+            <li className='text-[11px] sm:text-[10px] md:text-xs lg:text-base xl:text-lg 2xl:text-xl font-bold'>{data.title2}</li>
+            </ul>
+            
           </div>)
           }
           
-
-
         </div>
         {/* card section end */}
       </div>
